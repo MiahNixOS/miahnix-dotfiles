21b5fcb773b4ef14475675be0c7879a902c61228
Miah Nix <miahnixos@gmail.com>
2026-01-22 21:38:31 -0500

    Misc

--- miah-main/configuration.nix
+++ miah-main/configuration.nix
@@ -19,13 +19,18 @@
     services.udisks2.enable = true;
 
   # Bootloader.
-    hardware.decklink.enable = true;
+    # hardware.decklink.enable = true;
     boot.kernelModules = [ "kvm-amd" ];
     programs.xwayland.enable = true;
     programs.virt-manager = {
      enable = true;
     };
+
     virtualisation.spiceUSBRedirection.enable = true;
+    users.groups.libvirtd.members = ["archgodot"];
+    nix.settings.download-buffer-size = 52488000;
+
+    #services.libvirtd.enable = true;
     virtualisation.libvirtd = {
         enable = true;
         onBoot = "start";
@@ -46,23 +51,6 @@
   };
 
   boot.loader.efi.canTouchEfiVariables = true;
-
-  #boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  #hardware.graphics.enable = true;
-
-  # services.xserver.videoDrivers = ["nvidia"];
-  #
-  # hardware.nvidia = {
-  #   enable = true;
-  #   modesetting.enable = true;
-  #   open = false;
-  #   package = config.boot.kernelPackages.nvidiaPackages.beta;
-  # };
-  #
-  # # Use latest kernel.
-  # #boot.kernelPackages = pkgs.linuxPackages_latest;
-
   boot.kernelParams = [ "nvidia-drm.fbdev=1" ];
   hardware.graphics.enable = true;
   services.xserver.videoDrivers = ["nvidia"];
@@ -76,30 +64,11 @@
     package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
  
-   # Enable XDG desktop portals
   xdg.portal = {
   	enable = true;
 	wlr.enable = true;
   };
 
- # programs.obs-studio = {
- #    enable = true;
- # 
- #    package = (
- #      pkgs.obs-studio.override {
- #        cudaSupport = true;
- #      }
- #    );
- # 
- #    plugins = with pkgs.obs-studio-plugins; [
- #      wlrobs
- #      obs-pipewire-audio-capture
- #      obs-vaapi
- #      obs-gstreamer
- #      obs-vkcapture
- #    ];
- #  };
-  
   hardware.bluetooth.enable = true;
   services.power-profiles-daemon.enable = true;
   services.upower.enable = true;
@@ -107,43 +76,22 @@
 
   environment.sessionVariables = {
     NIXOS_OZONEWL=1;
-    QT_QPA_PLATFORM = "wayland";
   };
+
   services.samba.enable = true;
   services.avahi.enable = true;
   services.tumbler.enable = true;
 
-
-
-  # PostgreSQL
   services.postgresql = {
     enable = true;
     package = pkgs.postgresql_18; # Use this line
-    # ... other settings
   };
 
-  #xdg.portal.extraPortals = with pkgs; [
-  #  xdg-desktop-portal-wlr # Essential for wlroots-based compositors like MangoWC
-  #];
-  # You might need to disable other portals if they cause conflicts
-  # xdg.portal.extraPortals = with pkgs; [ ]; # Example if you need to clear defaults first
-  #xdg.portal.wlr.enable = true; # Specific setting for the wlr portal
-
-
   networking.hostName = "miahnix"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Enable networking
   networking.networkmanager.enable = true;
 
-  # Set your time zone.
   time.timeZone = "America/New_York";
 
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
@@ -164,13 +112,7 @@
     root = "/var/www/html"; # Or your chosen root directory
   };
 
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
-  #programs.xwayland.satellite.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
@@ -183,16 +125,13 @@
   	enable = true;
   };
 
-  # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "";
   };
 
-  # Enable CUPS to print documents.
   services.printing.enable = true;
 
-  # Enable sound with pipewire.
   services.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -203,42 +142,33 @@
     wireplumber.enable = true;
     # If you want to use JACK applications, uncomment this
     #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Use fish shell - ArchGodot
   programs.fish.enable = true;
+  programs.starship.enable = true;
   environment.shells = with pkgs; [ fish ];
 
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.archgodot = {
     isNormalUser = true;
     description = "ArchGodot";
-    extraGroups = [ "docker" "networkmanager" "wheel" "libvirtd" ];
+    extraGroups = [ "docker" "networkmanager" "wheel" "libvirtd" "virsh" ];
 
     shell = pkgs.fish;
 
     packages = with pkgs; [
       kdePackages.kate
-    #  thunderbird
     ];
   };
 
   services.open-webui.enable = true;
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
-  nixpkgs.config.allowUnfree = true;
+  nixpkgs.config = {
+    cudaSupport = true;
+    allowUnfree = true;
+  };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
@@ -255,18 +185,24 @@
     })
     vim
     tmux
-    #neovim
     bat
     btop
     neofetch
-    # obs-studio
     cmatrix
-    # obs-studio-plugins.wlrobs
     docker
     docker-compose
     lazydocker
     tor-browser
     vlc
+    starship
+    emacs
+    ripgrep
+    fd
+    libvterm
+    audacity
+    hyprshot
+    qt6.qtwayland
+    qt5.qtwayland
     nix-ld
     xwayland-satellite
     libappindicator
@@ -275,12 +211,6 @@
     davinci-resolve-studio
     gparted
     systrayhelper
-    # fish
-    # koncentro
-    # pomotroid
-    # pomodoro-gui
-    # pomatez
-    # pomodorolm
     blueman
     solaar
     logiops
@@ -288,7 +218,6 @@
     mongosh
     virt-manager
     dnsmasq
-    #luarocks
     yt-dlp
     lynx
     ollama
@@ -307,17 +236,13 @@
     pwvucontrol
     pulseaudio
     pipewire
-
     rustup
     opencode
-
     xdg-desktop-portal-wlr
     cloudflared
     rustdesk-flutter
     wget
     wayvnc
-
-
     kitty
     foot
     wmenu
@@ -332,6 +257,8 @@
   	nerd-fonts.jetbrains-mono
   ];
 
+  programs.nix-ld.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
@@ -340,30 +267,22 @@
   #   enableSSHSupport = true;
   # };
 
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
   services.openssh = {
     enable = true;
     settings.PasswordAuthentication = false;
   };
 
+  networking.firewall.enable = true;
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 
     80
     1234
   ];
-
   # networking.firewall.allowedUDPPorts = [ ... ];
+
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
